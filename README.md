12884fbb119f4d2883577cf460ef3147e56476a7
Robson Cruz <RobsonCruz.DXC@edp.com>
2021-06-08 07:08:49 +0100

    update

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ```bash
 bash <(curl -s https://raw.githubusercontent.com/robsonscruz/01-infra-docker/master/init.sh)
 ```
+
+## Example backend
+```bash
+wget https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war -O /home/ec2-user/backend/ROOT.war
+```
--- init.sh
+++ init.sh
@@ -11,6 +11,8 @@ sudo chmod a+x /home/ec2-user/docker-compose && sudo mv /home/ec2-user/docker-co
 # Template
 curl -sSL https://raw.githubusercontent.com/robsonscruz/01-infra-docker/master/docker-compose.yml > /home/ec2-user/docker-compose.yml
 mkdir -p /home/ec2-user/www && chmod 777 -R /home/ec2-user/www
+mkdir -p /home/ec2-user/backend && chmod 777 -R /home/ec2-user/backend
+wget https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war -O /home/ec2-user/backend/ROOT.war
 
 # Start container
 sudo /usr/local/bin/docker-compose -f /home/ec2-user/docker-compose.yml up -d
