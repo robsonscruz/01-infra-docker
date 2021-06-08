e2e3186c395e6563e2d1ca1f12c5742b96b79821
Robson Cruz <RobsonCruz.DXC@edp.com>
2021-06-08 22:23:03 +0100

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# SSM Auth
+cd /tmp
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
